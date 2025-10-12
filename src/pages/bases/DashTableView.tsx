67448cf923f1c92b37a31c244f0890d3c48a17e2
liufree <liufreeo@gmail.com>
2025-10-12 14:04:59 +0800

    feat: support bases

--- src/pages/bases/DashTableView.tsx
+++ src/pages/bases/DashTableView.tsx
@@ -31,11 +31,11 @@ export class DashTableView extends BasesView {
 					title: property,
 					dataIndex: property,
 					render: (_, record) => {
-						const value = record[property];
-						if (value === null || value === undefined) {
+						const data = record[property];
+						if (data === null || data === undefined) {
 							return "";
 						}
-						return ellipsisDisplay(value);
+						return ellipsisDisplay(data?.display);
 					}
 				};
 			}
@@ -63,13 +63,22 @@ export class DashTableView extends BasesView {
 				console.log(item.getValue(prop));
 
 				const {type, name} = parsePropertyId(prop);
+				console.log(type, name);
 				// `entry.getValue` returns the evaluated result of the property
 				// in the context of this entry.
 				const value = item.getValue(prop);
-				newItem[prop] = value?.toString();
-			});
+
+				const rowData = {
+					type: type,
+					name: name,
+					display: value
+				}
+				newItem[prop] = rowData
+			})
+
 			newDatas.push(newItem);
-		})
+
+		});
 
 		const columnsHead = this.createColumns(this.data.properties);
 
@@ -81,95 +90,95 @@ export class DashTableView extends BasesView {
 		root.render(<TableView app={app} source={""} columnsHead={columnsHead} data={newDatas}/>);
 	}
 
-
 }
 
-const TableView: React.FC<ViewProps> = ({app, source, columnsHead, data}) => {
+const
+	TableView: React.FC<ViewProps> = ({app, source, columnsHead, data}) => {
 
-	const [columns, setColumns] = React.useState<ProColumns<any>[]>([]);
-	const dv = getAPI(app);
+		const [columns, setColumns] = React.useState<ProColumns<any>[]>([]);
+		const dv = getAPI(app);
 
 
-	function parseTableResult(rows: any, params: any): Array<Record<string, any>> {
-		delete params.current;
-		delete params.pageSize;
+		function parseTableResult(rows: any, params: any): Array<Record<string, any>> {
+			delete params.current;
+			delete params.pageSize;
 
-		const filteredData = rows.filter(item => {
-			return Object.keys(params).every(key => {
-				if (params[key]) {
-					if (item[key]) {
-						return item[key].toString().toLowerCase().includes(params[key].toString().toLowerCase());
-					} else {
-						return true;
+			const filteredData = rows.filter(item => {
+				return Object.keys(params).every(key => {
+					if (params[key]) {
+						if (item[key]) {
+							return item[key].toString().toLowerCase().includes(params[key].toString().toLowerCase());
+						} else {
+							return true;
+						}
 					}
-				}
-				return true;
-			});
-		});
-
-		return filteredData;
-	}
-
-	const executeTableQuery = async (datas: any, params: any) => {
-		const sql = source;
-
-		const queryResult = datas
-
-		const tableData: any = parseTableResult(queryResult, params);
-
-		const columns = columnsHead;
-
-		return {tableData: tableData, columns: columns};
-	}
-
-	useEffect(() => {
-		//	console.log("source", source);
-	}, []);
-
-
-	return (
-		<ProTable
-			scroll={{x: 'max-content'}}
-			cardBordered
-			editable={{
-				type: 'multiple',
-			}}
-			columns={columns}
-			//	dataSource={data}
-			columnsState={{
-				persistenceKey: 'pro-table-singe-demos',
-				persistenceType: 'localStorage',
-				defaultValue: {
-					option: {fixed: 'right', disable: true},
-				},
-				onChange(value) {
-					//	console.log('value: ', value);
-				},
-			}}
-			rowKey={(record) => record.key}
-			search={{
-				labelWidth: 'auto',
-			}}
-			pagination={{
-				showSizeChanger: true,
-			}}
-			headerTitle="TableView"
-			request={async (params, sort, filter) => {
-				//	console.log('params:', params);
-				const response = await executeTableQuery(data, params);
-
-				const {tableData, columns} = response;
-
-				setColumns(columns);
-				return Promise.resolve({
-					data: tableData,
-					success: true,
-					total: tableData.length,
-					//	columns: columns,
+					return true;
 				});
-			}}
-		/>
-	);
-};
+			});
+
+			return filteredData;
+		}
+
+		const executeTableQuery = async (datas: any, params: any) => {
+			const sql = source;
+
+			const queryResult = datas
+
+			const tableData: any = parseTableResult(queryResult, params);
+
+			const columns = columnsHead;
+
+			return {tableData: tableData, columns: columns};
+		}
+
+		useEffect(() => {
+			//	console.log("source", source);
+		}, []);
+
+
+		return (
+			<ProTable
+				scroll={{x: 'max-content'}}
+				cardBordered
+				editable={{
+					type: 'multiple',
+				}}
+				columns={columns}
+				//	dataSource={data}
+				columnsState={{
+					persistenceKey: 'pro-table-singe-demos',
+					persistenceType: 'localStorage',
+					defaultValue: {
+						option: {fixed: 'right', disable: true},
+					},
+					onChange(value) {
+						//	console.log('value: ', value);
+					},
+				}}
+				rowKey={(record) => record.key}
+				search={{
+					labelWidth: 'auto',
+				}}
+				pagination={{
+					showSizeChanger: true,
+				}}
+				headerTitle="TableView"
+				request={async (params, sort, filter) => {
+					//	console.log('params:', params);
+					const response = await executeTableQuery(data, params);
+
+					const {tableData, columns} = response;
+
+					setColumns(columns);
+					return Promise.resolve({
+						data: tableData,
+						success: true,
+						total: tableData.length,
+						//	columns: columns,
+					});
+				}}
+			/>
+		);
+	};
 
 
